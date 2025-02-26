b6537c70d164140b7dd52f783a369e56ff08c275
Alexander-Bakshaev <shuriknt@gmail.com>
2025-02-26 21:54:43 +0500

    slug 2

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,9 +1,11 @@
+from tkinter.font import names
+
 from django.shortcuts import render, get_object_or_404
 from .models import Movie
 
 
 def sow_all_movies(request):
-    movies = Movie.objects.all()
+    movies = Movie.objects.order_by('name')[:3]
     return render(request, 'movie_app/all_movies.html', {'movies': movies})
 
 
