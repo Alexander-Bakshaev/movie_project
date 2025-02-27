1b0f390eb5a333f6704f9c6c25658d7bb30ce24b
Alexander-Bakshaev <shuriknt@gmail.com>
2025-02-27 13:20:14 +0500

    функция aggregate

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,12 +1,16 @@
 from tkinter.font import names
-
+from django.db.models import F, Max, Min, Avg, Sum, Count
 from django.shortcuts import render, get_object_or_404
 from .models import Movie
 
 
 def sow_all_movies(request):
-    movies = Movie.objects.order_by('name')[:3]
-    return render(request, 'movie_app/all_movies.html', {'movies': movies})
+    movies = Movie.objects.order_by('name')
+    agg = movies.aggregate(Avg('budget'), Max('rating'), Min('rating'), Count('id'))
+    return render(request, 'movie_app/all_movies.html', {
+        'agg': agg,
+        'movies': movies
+    })
 
 
 def show_one_movie(request, slug_movie: str):
